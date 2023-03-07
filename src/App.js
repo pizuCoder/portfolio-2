63dac9ae7cb9636ae558c7dcd947fc8a820f1977
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2023-03-07 13:50:07 +0530

    another attempt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar/NavBar";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  return(
+    <>
+  <NavBar isDarkMode={isDarkMode}
+  toggleDarkMode={() => setIsDarkMode(!isDarkMode)}
+  />
+  </>
+  )
+  
 }
-
-export default App;
+export default App
